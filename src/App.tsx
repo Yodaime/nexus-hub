732b340179644e0c10cad546c305e5ebc46d914c
Odair Silva Carvalho <37254416+Yodaime@users.noreply.github.com>
2026-03-24 02:45:40 -0300

    retirada de login

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
+// Autenticação removida
 import TasksPage from "./pages/TasksPage";
 import FinancesPage from "./pages/FinancesPage";
 import HabitsPage from "./pages/HabitsPage";
@@ -11,9 +11,7 @@ import RemindersPage from "./pages/RemindersPage";
 import GoalsPage from "./pages/GoalsPage";
 import OrganizePage from "./pages/OrganizePage";
 import NotFound from "./pages/NotFound";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import { ProtectedRoute } from "./components/ProtectedRoute";
+// Autenticação removida
 
 const queryClient = new QueryClient();
 
@@ -23,62 +21,16 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter future={{ v7_startTransition: true }}>
-        <AuthProvider>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <TasksPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/finances"
-              element={
-                <ProtectedRoute>
-                  <FinancesPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/habits"
-              element={
-                <ProtectedRoute>
-                  <HabitsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reminders"
-              element={
-                <ProtectedRoute>
-                  <RemindersPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/goals"
-              element={
-                <ProtectedRoute>
-                  <GoalsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/organize"
-              element={
-                <ProtectedRoute>
-                  <OrganizePage />
-                </ProtectedRoute>
-              }
-            />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AuthProvider>
+        <Routes>
+          <Route path="/" element={<TasksPage />} />
+          <Route path="/finances" element={<FinancesPage />} />
+          <Route path="/habits" element={<HabitsPage />} />
+          <Route path="/reminders" element={<RemindersPage />} />
+          <Route path="/goals" element={<GoalsPage />} />
+          <Route path="/organize" element={<OrganizePage />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
